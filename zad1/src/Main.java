4695651ca17dbd4461708f92779f6905f2525742
MikolajKrolak <s35908@pjwstk.edu.pl>
2026-03-07 13:07:51 +0100

    Add files via upload

---
+++ zad1/src/Main.java
@@ -0,0 +1,18 @@
+import static java.lang.Math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //zad1
+        double a = 2;
+        double b = 2;
+
+        System.out.println(a + b);
+
+        //zad2
+        double c = PI;
+        System.out.print(pow(c, 2));
+
+        //zad3
+
+    }
+}
