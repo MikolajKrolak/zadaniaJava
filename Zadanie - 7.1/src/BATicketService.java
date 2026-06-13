07c8f5329ba0f3ee89efd4f853457f49bf598cd4
MikolajKrolak <s35908@pjwstk.edu.pl>
2026-06-13 12:39:00 +0200

    Add files via upload

---
+++ Zadanie - 7.1/src/BATicketService.java
@@ -0,0 +1,6 @@
+import java.util.Date;
+import java.util.List;
+
+public interface BATicketService {
+    List<BATicket> getTicketInfo(Airport from, Airport to, Date when);
+}
---
+++ Zadanie - 7.1/src/UATicketInfo.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class UATicketInfo {
+    String from;
+    String to;
+    Date dateTime;
+    double price;
+}
